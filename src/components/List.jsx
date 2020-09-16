841c5cc2b6d056e3d15a91c92b7dfb8f755e7003
TeddyRod <teddy.rogers.pro@outlook.com>
2020-09-17 01:47:11 +0200

    display the results from the searchbar component in a list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -8,7 +8,19 @@ function List({currentList, setMovie}) {
 
         return (
                 <div>
-
+                        {currentList && currentList.map(oneMovie => (
+                                <div key={oneMovie.id} onClick={() => selectMovie(oneMovie)}>
+                                        <div>
+                                                <div style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w185/${oneMovie.poster_path})`}} />
+                                                <span>{oneMovie.title}</span>
+                                        </div>
+                                        <div>
+                                                <span>{oneMovie.vote_average}</span>
+                                                <span>{oneMovie.popularity}</span>
+                                                <span>{oneMovie.release_date}</span>
+                                        </div>
+                                </div>
+                        ))}
                 </div>
         )
 }

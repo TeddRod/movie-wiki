7a303e618a688d1e5d44f9fafef87d3a7fdcfe8b
TeddyRod <teddy.rogers.pro@outlook.com>
2020-09-17 09:32:13 +0200

    structure of the movie details components

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
-function MovieDetails() {
+function MovieDetails({currentMovie}) {
         return (
                 <div>
-                        
+                        {currentMovie && (
+                                <>
+                                <section>
+                                        <div style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${currentMovie.poster_path})`}}/>
+                                        <div>
+                                                <div>{currentMovie.vote_average} <span>★</span> <span>{currentMovie.vote_count}</span></div>
+                                                <div>
+                                                        <span>{currentMovie.title}</span> <span>{currentMovie.original_language}</span>
+                                                </div>
+                                                <div>{currentMovie.release_date}</div>
+                                        </div>
+                                </section>
+
+                                <section>
+                                        <div>
+                                                <span>Overview :</span>
+                                                <span>{currentMovie.overview}</span>
+                                        </div>
+                                </section>
+                                </>
+                        )}
                 </div>
         )
 }

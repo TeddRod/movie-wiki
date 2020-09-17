3abc013a0d90045628c49d2224af48945255f878
TeddyRod <teddy.rogers.pro@outlook.com>
2020-09-18 01:23:37 +0200

    structure of the actor component

--- src/components/ActorDetails.jsx
+++ src/components/ActorDetails.jsx
@@ -18,7 +18,52 @@ function ActorDetails({currentActor}) {
         return (
                 <div>
                      {actorDetails && (
-                                
+                                <div id="actorSection">
+                                        <section id="actorIdentity">
+                                                <div id="actorPix" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${actorDetails.image})` }}/>
+                                                <div id="actorInfo">
+                                                        <span className="fontBigger clrGreen">{actorDetails.name}</span>
+                                                        <div className="fontBig clrBlue"><span className="fontRegularBold clrBlack">popularity: </span><span>{actorDetails.popularity}</span>  <span className="star">★</span></div>
+                                                        <span>{actorDetails.birthday}</span>
+                                                </div>
+                                        </section>
+
+                                        {actorDetails.bio && (
+                                                <section id="actorBio">
+                                                                <div id="bioSection">
+                                                                        <span className="clrBlue fontBig">Biography :</span>
+                                                                        <span className="clrGrey fontRegular">{actorDetails.bio}</span>
+                                                                </div>
+                                                </section>
+                                        )}
+
+                                        {actorDetails.movie_credits && (
+                                                <section id="actorMovies">
+                                                        <span>movies :</span>
+                                                        {actorDetails.movie_credits.map(eachMovie => (
+                                                                <div>
+                                                                        <span>{eachMovie.character}</span>
+                                                                        <span>{eachMovie.title}</span>
+                                                                        <span>{eachMovie.release_date}</span>
+                                                                </div>
+                                                        ))}
+                                                </section>
+                                        )}
+                                        
+                                        {actorDetails.tv_credits && (
+                                                <section id="actorTvShows">
+                                                        <span>tv shows :</span>
+                                                        {actorDetails.tv_credits.map(eachShow => (
+                                                                <div>
+                                                                        <span>{eachShow.character}</span>
+                                                                        <span>{eachShow.title}</span>
+                                                                        <span>{eachShow.first_air_date}</span>
+                                                                </div>
+                                                        ))}
+                                                </section>
+                                        )}
+                                </div>
+                     )}
                 </div>
         )
 }

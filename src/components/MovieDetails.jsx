9e77c061d44b0e127d4545a89ce6baed278b759c
TeddyRod <teddy.rogers.pro@outlook.com>
2020-09-17 15:39:26 +0200

    details css done

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 
 function MovieDetails({currentMovie}) {
         return (
-                <div id="detailsSection">
+                <div id="detailsSection" className="white-background">
                         {currentMovie && (
                                 <>
-                                <section>
-                                        <div style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${currentMovie.poster_path})`}}/>
+                                <section id="mainSection">
+                                        <div id="detailPoster" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${currentMovie.poster_path})`}}/>
                                         <div>
-                                                <div>{currentMovie.vote_average} <span>★</span> <span>{currentMovie.vote_count}</span></div>
-                                                <div>
-                                                        <span>{currentMovie.title}</span> <span>{currentMovie.original_language}</span>
+                                                <div className="fontBig clrBlack"><span className="clrBlue">{currentMovie.vote_average}</span> <span className="star">★</span>, votes: <span className="clrBlue">{currentMovie.vote_count}</span></div>
+                                                <div id="detailTitle">
+                                                        <span className="clrGreen fontBigger">{currentMovie.title}</span> <span className="clrGrey fontMicro"> {currentMovie.original_language}</span>
                                                 </div>
-                                                <div>{currentMovie.release_date}</div>
+                                                <div className="clrGrey fontMicro">{currentMovie.release_date}</div>
                                         </div>
                                 </section>
 
                                 <section>
-                                        <div>
-                                                <span>Overview :</span>
-                                                <span>{currentMovie.overview}</span>
+                                        <div id="overviewSection">
+                                                <span className="clrBlack fontBig">Overview :</span>
+                                                <span className="clrGrey fontRegular">{currentMovie.overview}</span>
                                         </div>
                                 </section>
                                 </>

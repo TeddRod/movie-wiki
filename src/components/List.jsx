2dbd0bca85a70f51292e0945312f873df6ecd687
TeddyRod <teddy.rogers.pro@outlook.com>
2020-09-17 14:59:13 +0200

    list css done

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -7,17 +7,18 @@ function List({currentList, setMovie}) {
         }
 
         return (
-                <div id="listDiv">
+                <div id="listDiv" className="white-background">
                         {currentList && currentList.map(oneMovie => (
-                                <div key={oneMovie.id} onClick={() => selectMovie(oneMovie)}>
-                                        <div>
-                                                <div style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w185/${oneMovie.poster_path})`}} />
-                                                <span>{oneMovie.title}</span>
+                                <div id="oneMovie" key={oneMovie.id} onClick={() => selectMovie(oneMovie)}>
+                                        <div id="pixNtitle">
+                                                <div id="moviePix" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w185/${oneMovie.poster_path})`}} />
                                         </div>
-                                        <div>
-                                                <span>{oneMovie.vote_average}</span>
-                                                <span>{oneMovie.popularity}</span>
-                                                <span>{oneMovie.release_date}</span>
+                                        <div id="movieInfo">
+                                                <span id="movieTitle" className="fontRegular">{oneMovie.title}</span>
+                                                <div id="movieRates">
+                                                        <span className="fontMicro clrGrey">{oneMovie.vote_average}<span className="star">★</span></span>
+                                                        <span className="fontMicro clrGrey">{oneMovie.release_date}</span>
+                                                </div>
                                         </div>
                                 </div>
                         ))}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -29,7 +29,7 @@ function SearchBar({setList}) {
         },[])
 
         return (
-                <div id="searchBarDiv" className="searchBar-background">
+                <div id="searchBarDiv" className="green-background">
                         <div id="inputDiv">
                                 <input id="input" className="clrGrey  fontMicro" type="text" value={textInput} onChange={(e) => searchMovies(e)} onClick={setToBlank}></input>
                                 <button id="button" onClick={eraseInput}>✕</button>

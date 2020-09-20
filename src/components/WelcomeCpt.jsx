6301d06c3c666dccd2f9231a661a33f64f5805b2
TeddyRod <teddy.rogers.pro@outlook.com>
2020-09-21 01:42:47 +0200

    refactor the welcome message

--- src/components/WelcomeCpt.jsx
+++ src/components/WelcomeCpt.jsx
@@ -4,7 +4,7 @@ function WelcomeCpt() {
         return (
                 <div id="welcomeSection">
                                 <span className="fontBigger clrBlack">Movie<span className="clrGreen"> Wiki</span>.</span>
-                                <span className="clrGrey fontRegularBold">Welcome to <span className="fontBig clrBlue">Movie Wiki</span>. Browse through the list of films presented in the left-hand menu, search for a particular title, and view the details of the feature film of your choice. Note that additional information is also available for each actor.</span>
+                                <span className="clrGrey fontRegularBold">Welcome to <span className="fontBig clrBlue">Movie Wiki</span>. Browse through all <span className="fontBig clrGreen">TheMovieDB </span>and the the results in the left-hand menu. Search for a particular title of movie, and view the details of the feature film of your choice. Note that additional information is also available for each actor.</span>
                                 <span className="fontRegularBold clrBlue">Good Navigation...</span>
                         
                 </div>
